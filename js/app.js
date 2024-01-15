3adc6922ed0f97f4e89ed3d6193d753605735be3
Pedromarts <pedromarts.93@gmail.com>
2024-01-15 16:27:33 -0300

    limpando código

--- js/app.js
+++ js/app.js
@@ -2,9 +2,5 @@
 function comprar() {
     let tipo = document.getElementById ('tipo-ingresso').value;
     let quantidade = document.getElementById ('qtd').value;
-    let lista = document.getElementById ('lista');
-    let selecaoLista = lista.split ("qnt")[1];
-    
-    alert (lista.value);
 
 }
