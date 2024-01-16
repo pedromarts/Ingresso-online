acc0de9fc5479a984a36c958a834de0d5a7293d8
Pedromarts <pedromarts.93@gmail.com>
2024-01-16 15:47:06 -0300

    Finalização do Projeto

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ function comprar() {
     let qtd = parseInt(document.getElementById ('qtd').value);
     if (tipo.value == 'pista'){
         comprarPista (qtd);
-
+    } else if (tipo.value == 'superior'){
+        comprarSuperior(qtd);
+    } else{
+        comprarInferior (qtd);
     }
 }
 
@@ -16,6 +19,32 @@ function comprarPista (qtd){
     } else {
         qtdPista = qtdPista - qtd; //esse é apenas o cálculo
         document.getElementById ('qtd-pista').textContent = qtdPista; // aqui é para atualizar a página, levar o valor
+        alert ('Compra realizada com sucesso')
+    }
+
+}
+
+function comprarSuperior (qtd){
+    let qtdSuperior= parseInt(document.getElementById ('qtd-superior').textContent);
+
+    if (qtd > qtdSuperior){
+        alert (' Quantidade indisponível para tipo Superior');
+    } else {
+        qtdSuperior= qtdSuperior - qtd; //esse é apenas o cálculo
+        document.getElementById ('qtd-superior').textContent = qtdSuperior; // aqui é para atualizar a página, levar o valor
+        alert ('Compra realizada com sucesso')
+    }
+
+}
+
+function comprarInferior (qtd){
+    let qtdInferior= parseInt(document.getElementById ('qtd-inferior').textContent);
+
+    if (qtd > qtdInferior){
+        alert (' Quantidade indisponível para tipo inferior');
+    } else {
+        qtdInferior= qtdInferior - qtd; //esse é apenas o cálculo
+        document.getElementById ('qtd-inferior').textContent = qtdInferior; // aqui é para atualizar a página, levar o valor
         alert ('Compra realizada com sucesso')
     }
 
