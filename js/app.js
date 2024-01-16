402eb81a326b86cf611d015833910847ac947e8a
Pedro Martins <pedromarts.93@gmail.com>
2024-01-15 22:11:57 -0300

    avanço na aula

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 
 function comprar() {
-    let tipo = document.getElementById ('tipo-ingresso').value;
-    let quantidade = document.getElementById ('qtd').value;
+    let tipo = document.getElementById ('tipo-ingresso');
+    let qtd = parseInt(document.getElementById ('qtd').value);
+    if (tipo.value == 'pista'){
+        comprarPista (qtd);
+
+    }
+}
+
+function comprarPista (qtd){
+    let qtdPista = parseInt(document.getElementById ('qtd-pista').textContent);
+
+    if (qtd > qtdPista){
+        alert (' Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd; //esse é apenas o cálculo
+        document.getElementById ('qtd-pista').textContent = qtdPista; // aqui é para atualizar a página, levar o valor
+        alert ('Compra realizada com sucesso')
+    }
 
 }
